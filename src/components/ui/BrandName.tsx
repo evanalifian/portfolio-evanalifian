69f2ca00fc75454303c0bdfb88465b5db8cae3b0
evanalifian <evanalifian@gmal.com>
2025-07-11 17:50:11 +0700

    adding border for year

--- src/components/ui/BrandName.tsx
+++ src/components/ui/BrandName.tsx
@@ -4,7 +4,7 @@ import getLastTwoDigit from "../../utils/getlastTwoDigit";
 export default function BrandName() {
   const year = new Date().getFullYear();
   return (
-    <span className="font-mono text-sm inline-flex items-center justify-baseline">
+    <span className="inline-flex cursor-default items-center justify-baseline font-mono text-sm">
       {data.alias}
       <span className="ms-1 inline-block rounded-md border border-zinc-700 px-1 py-0.5 text-[10px]">
         {getLastTwoDigit(year)}

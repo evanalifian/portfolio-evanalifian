5f8204ac80c04fa0412b5f75bef28501503cc8ba
evanalifian <evanalifian@gmal.com>
2025-07-11 18:09:46 +0700

    adding jumbotron compenent

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/layout/Header";
+import Jumbotron from "./components/layout/Jumbotron";
 
 export default function App() {
   return (
     <>
       <Header />
+      <main className="mx-auto w-full max-w-5xl px-4">
+        <Jumbotron />
+      </main>
     </>
   );
 }

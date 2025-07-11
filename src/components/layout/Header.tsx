81cd1998944e6ac8a521dafc36c42d67c87c3e01
evanalifian <evanalifian@gmal.com>
2025-07-11 17:49:35 +0700

    change font size and dursor

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,11 @@
-import data from "../../../data.json"
+import data from "../../../data.json";
 import BrandName from "../ui/BrandName";
 
 export default function Header() {
   return (
     <header className="fixed top-0 w-full border-b border-dashed border-zinc-300 bg-white">
-      <div className="mx-auto flex w-full max-w-5xl items-center justify-between p-4">
-        <span className="text-lg font-medium text-gray-700 transition-colors hover:text-black">
+      <div className="mx-auto flex h-14 w-full max-w-5xl items-center justify-between px-4">
+        <span className="cursor-pointer text-xl font-medium text-gray-700 transition-colors hover:text-black">
           {data.nickname}
         </span>
         <BrandName />
